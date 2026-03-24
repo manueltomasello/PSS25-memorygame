d77de34041a38e84b3c3f52d8fc172ef7c3fe720
manueltomasello <manuel.tomasello@studio.unibo.it>
2026-03-24 19:43:51 +0100

    Svil Board e Main
    board gestisce le logiche del "tabellone" di carte che poi è un array, sviluppo di un main provvisorio per debug, nessun errore (apparte i metodi vuoti con //todo)

--- src/main/java/it/unibo/memory/controller/Main.java
+++ src/main/java/it/unibo/memory/controller/Main.java
@@ -1,5 +1,15 @@
 package it.unibo.memory.controller;
 
+import it.unibo.memory.model.Board;
+import it.unibo.memory.model.Difficulty;
+
 public class Main {
-    
+    public static void main(final String[] args) {
+        Difficulty diff = Difficulty.MEDIUM;
+        Board board = new Board(diff);
+        System.out.println("Righe: " + diff.getRows());
+        System.out.println("Colonne: " + diff.getCols());
+        System.out.println("Totale carte: " + diff.totalCards());
+        System.out.println("Tutte abbinate? " + board.allMatched());
+    }
 }
--- src/main/java/it/unibo/memory/model/Board.java
+++ src/main/java/it/unibo/memory/model/Board.java
@@ -1,5 +1,34 @@
 package it.unibo.memory.model;
- 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Board {
-    // TODO
+
+    private final List<Card> cards;
+
+    //Board prende la matrice dalla difficoltà scelta 
+    public Board(final Difficulty diff) {
+        this.cards = new ArrayList<>();
+        genCards(diff.totalPairs());
+    }  
+    //Organizza e mescola la lista delle carte.
+   
+    private void genCards(final int pairs) {
+        for (int symbol = 0; symbol < pairs; symbol++) {
+            cards.add(new Card(symbol));
+            cards.add(new Card(symbol));
+        }
+        Collections.shuffle(cards); // mischio le carte dentro la lista randomico
+    }
+
+    public Card getCard(final int index) {
+        return cards.get(index); //Restituisce l'indice della carta
+    }
+
+    //meccanismo di vittoria, ci serve per vedere quando tutte le coppie sono matchate
+    public boolean allMatched() {
+        return cards.stream().allMatch(Card::isMatched);
+    }
 }
