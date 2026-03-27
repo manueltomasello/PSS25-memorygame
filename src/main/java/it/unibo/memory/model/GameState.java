6181c94f2d2fd13146a718e15d56536b5bc26fd0
Marco Toma <marco.toma3@studio.unibo.it>
2026-03-27 16:19:36 +0100

    Creazione del GameState
    Aggiunto GameState con stati base della partita

--- src/main/java/it/unibo/memory/model/GameState.java
+++ src/main/java/it/unibo/memory/model/GameState.java
@@ -1,5 +1,8 @@
 package it.unibo.memory.model;
- 
-public class GameState {
-    // TODO
+
+// Rappresenta i possibili stati della partita
+public enum GameState {
+    WAITING,    // aspetto il primo click
+    FIRST_CARD, // ho girato la prima carta
+    GAME_OVER   // partita finita
 }
